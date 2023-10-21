79db47747b445ff6d27a7fbd86d2d96fc5c276f1
Joud Awad <joud.awad@metalabdesign.com>
2023-10-21 16:21:02 +0300

    first comment

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+const app = express();
+const PORT = 3000;
+
+app.get("/auth/user", (req, res) => {
+  if (req.headers["x-auth-header"] === "allow") {
+    return res.status(200).json({ message: "allowed" });
+  }
+  return res.status(401).json({ message: "not allowed to enter" });
+});
+
+app.get("/auth-admin", (req, res) => {
+  if (req.headers["x-auth-header"] === "allow") {
+    return res.status(200).json({ message: "allowed" });
+  }
+  return res.status(401).json({ message: "not allowed to enter" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
